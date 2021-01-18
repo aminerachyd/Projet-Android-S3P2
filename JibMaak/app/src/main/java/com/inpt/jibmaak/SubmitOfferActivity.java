7bc15cbcd88c087c6e6145bd00c6b444e66407c1
Amine Rachyd <aminerachyd99@gmail.com>
2021-01-18 15:11:49 +0100

    Activité proposer une offre

---
+++ JibMaak/app/src/main/java/com/inpt/jibmaak/SubmitOfferActivity.java
@@ -0,0 +1,17 @@
+package com.inpt.jibmaak;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SubmitOfferActivity extends AppCompatActivity {
+
+    // Activité pour proposer une offre
+    // L'utilisateur (livreur) peut saisir les informations pour sa proposition d'offre
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submit_offer);
+    }
+}
