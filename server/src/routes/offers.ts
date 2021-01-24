5b4a6f1213cd4a5601ef22201d5ac7008154e64f
Amine Rachyd <aminerachyd99@gmail.com>
2021-01-24 22:38:18 +0100

    Ajout du filtrage dans la route /offers
    Dans POST /offers:
    On peut filtrer les offres à afficher.
    Les filtre en JSON sont :
        {depart,
        destination,
        departAvant,
        departApres,
        arriveAvant,
        arriveApres,
        minPrixKg,
        maxPrixKg,
        minPoidsDisponible,
        maxPoidsDisponible}
    
    La pagination et la limite sont disponibles également.

--- server/src/routes/offers.ts
+++ server/src/routes/offers.ts
@@ -1,14 +1,13 @@
 import express from "express";
-import OfferModel from "../models/Offer";
 import UserModel from "../models/User";
-import { userInfos } from "../utils/helpers";
+import OfferModel from "../models/Offer";
+import { userInfos, filterOffers } from "../utils/helpers";
 
 // Limite par défaut d'offres à récupérer par requete
 const MAX_LIMIT = 15;
 
 const router = express.Router();
 
-// TODO Have to implement pagination, limit and offset
 /**
  * Route pour récupérer toutes les offres
  * ROUTE: /offers
@@ -35,24 +34,87 @@ router.get("/", async (req, res) => {
       ? limitNumber * parseInt(<string>page) - limitNumber
       : 0;
 
-  // Critères de recherche
-  const {
-    depart,
-    destination,
-    departAvant,
-    departApres,
-    arriveAvant,
-    arriveApres,
-    minPrixKg,
-    maxPrixKg,
-    minPoidsDisponible,
-    maxPoidsDisponible,
-  } = req.body;
-
   try {
     const result = await OfferModel.find()
       .limit(limitNumber + 1)
       .skip(pageNumber);
+
+    let offers: any = [];
+
+    let hasMore = result.length === limitNumber + 1;
+
+    for (let i = 0; i < limitNumber; i++) {
+      let offer = result[i];
+      if (!offer) {
+        break;
+      }
+
+      let {
+        id,
+        user,
+        lieuDepart,
+        lieuArrivee,
+        dateDepart,
+        dateArrivee,
+        prixKg,
+        poidsDispo,
+      } = offer;
+
+      let userData = await UserModel.findById(user._id);
+
+      offers.push({
+        id,
+        user: userInfos(userData),
+        lieuDepart,
+        lieuArrivee,
+        dateDepart,
+        dateArrivee,
+        prixKg,
+        poidsDispo,
+      });
+    }
+
+    res.send({
+      message: "Offres récupérées",
+      payload: { hasMore, offers, page: realPageNumber, limit: limitNumber },
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).send({
+      error: "Erreur du serveur",
+    });
+  }
+});
+
+/**
+ * Route pour récupérer toutes les offres avec filtres
+ * ROUTE: /offers
+ * METHOD: POST
+ * RETURN: Tableau d'offres, un boolean hasMore qui indique si il y'a plus de données à récupérer, la page et la limite
+ */
+router.post("/", async (req, res) => {
+  // On récupère la page et le limit depuis la requete
+  const { page, limit } = req.query;
+
+  /**
+   * On évalue la limite et la page
+   * La limite ne doit pas dépasser la limite par défaut
+   * La page est calculée en fonction de la limite évaluée
+   */
+  let limitNumber = parseInt(<string>limit)
+    ? Math.min(parseInt(<string>limit), MAX_LIMIT)
+    : MAX_LIMIT;
+
+  let realPageNumber = page ? parseInt(<string>page) : 1;
+
+  let pageNumber =
+    parseInt(<string>page) > 1
+      ? limitNumber * parseInt(<string>page) - limitNumber
+      : 0;
+
+  try {
+    const result = await filterOffers(req.body);
 
     let offers: any = [];
 
--- server/src/utils/helpers.ts
+++ server/src/utils/helpers.ts
@@ -1,4 +1,5 @@
 import crypto from "crypto";
+import OfferModel from "../models/Offer";
 const HASH_SECRET = process.env.HASH_SECRET;
 
 // Fonction pour hasher un mot de passe
@@ -19,4 +20,37 @@ export const hash = (str: string): string | false => {
 export const userInfos = (user: any) => {
   const { id, email, nom, prenom, telephone } = user;
   return { id, email, nom, prenom, telephone };
+};
+
+// Fonction pour filtrer les offres
+export const filterOffers = async (args) => {
+  let {
+    depart,
+    destination,
+    departAvant,
+    departApres,
+    arriveAvant,
+    arriveApres,
+    minPrixKg,
+    maxPrixKg,
+    minPoidsDisponible,
+    maxPoidsDisponible,
+  } = args;
+
+  let query = OfferModel.find();
+
+  depart && query.where("lieuDepart").equals(depart);
+  destination && query.where("lieuArrivee").equals(destination);
+  departAvant && query.where("dateDepart").lte(departAvant);
+  departApres && query.where("dateDepart").gte(departApres);
+  arriveAvant && query.where("dateArrivee").lte(arriveAvant);
+  arriveApres && query.where("dateArrivee").gte(arriveApres);
+  minPrixKg && query.where("prixKg").gte(minPrixKg);
+  maxPrixKg && query.where("prixKg").lte(maxPrixKg);
+  minPoidsDisponible && query.where("poidsDispo").gte(minPoidsDisponible);
+  maxPoidsDisponible && query.where("poidsDispo").lte(maxPoidsDisponible);
+
+  const result = await query.exec();
+
+  return result;
 };
